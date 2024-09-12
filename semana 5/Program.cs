07c444649d0a195a3bab9c03773459affb95243e
SebastianGarb <sebastiangarboza63@hotnmail.com>
2024-09-12 10:04:35 -0500

    first commit

--- semana 5/Program.cs
+++ semana 5/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int ;
+using System.ComponentModel;
+Console.WriteLine("ALO");
+string re = "s";
+int suma = 0, cant = 0;
+while (re == "s")
+{
+    Console.WriteLine("Desea participar? (s/n)");
+    re = Console.ReadLine().ToLower().Trim();
+    if ( re == "s")
+    {
+        Console.WriteLine("Ingrese edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        suma = suma + edad;
+        cant = cant + 1;
+    }
+}    
+double prom = suma / cant;
+Console.WriteLine($"El promedio de {cant} edades es {prom}");
 
-Console.WriteLine("");
+
+
